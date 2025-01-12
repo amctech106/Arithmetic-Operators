aaeae27cf056517b58f9927dd577641d66f60b21
amctech106 <121874402+amctech106@users.noreply.github.com>
2025-01-12 11:55:34 +0500

    Add files via upload

---
+++ Arithmetic Operators/app.js
@@ -0,0 +1,37 @@
+
+//Arithmetic Operators -----------
+// let a = 5;
+// let b = 3; 
+// console.log ("a + b =", a +b);
+// console.log ("a + b =", a +b);
+// console.log ("a * b=", a * b);
+// console.log ("a / b =", a / b);
+// console.log ("a % b =", a % b); 
+// console.log ("a ** b =", a ** b); 
+
+//Unary Operator ------------------
+
+// let a = 5;
+// let b = 3; 
+// console.log ("a =", a , "b = ", b);
+
+// a = a +1; 
+// console.log("a = ", a)
+// a = a -1; 
+// console.log("a = ", a)
+
+//Pre Increament------& Pre Decreament-----------
+
+let a = 5;
+let b = 3; 
+
+//++a; 
+//console.log("a = ", a); 
+
+--a; 
+console.log("a = ", a); 
+
+
+
+
+
